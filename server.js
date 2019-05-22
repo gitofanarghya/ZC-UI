1bbea31cbfa96a3f227ffa3957ede26b9a559390
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2019-05-23 01:03:22 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://localhost:1883')
 
 client.on('connect', function () {
+  console.log('connected to mqtt broker')
   client.subscribe('xbee/response', function (err) {
     if(!err) {
-      console.log('subscribed to xbee/response')
+      console.log('subscribed to mqtt topic xbee/response')
     }
   })
 })
 
-client.on('message', function (topic, message) {
-  console.log('inside on message', message.toString(), topic.toString())
-})
-
-client.on('xbee/response', function (topic, message) {
-  console.log('inside on xbee/response', message.toString(), topic.toString())
+io.on('connection', function(socket) {
+  console.log('client connected to socket server')
+  client.on('message', function(topic, message) {
+    console.log(`received message from mqtt broker topic = ${topic} and message = ${message}`)
+    socket.emit('message', message)
+  })
 })
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -27,7 +30,7 @@ app.get('/*', function (req, res) {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
