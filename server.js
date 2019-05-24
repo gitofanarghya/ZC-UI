4ad1b5fd330aa9d40703eae489a687620fc7980c
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2019-05-24 16:54:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,11 +45,11 @@ io.on('connection', function(socket) {
 })
 
 
-/* app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-}); */
+});
 
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
