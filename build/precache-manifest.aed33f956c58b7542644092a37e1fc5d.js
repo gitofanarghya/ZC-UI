628bcef418642f928cb17a82b3960aede5a6b384
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2019-05-29 19:29:14 +0530

    build

---
+++ build/precache-manifest.aed33f956c58b7542644092a37e1fc5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e61ee62b2716d2cbff8f1b89757a0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d1ae5faae32abb98993",
+    "url": "/static/css/main.d121bd1c.chunk.css"
+  },
+  {
+    "revision": "5d3c22e3616c63f4dbfe",
+    "url": "/static/js/2.e26ec0ed.chunk.js"
+  },
+  {
+    "revision": "2d1ae5faae32abb98993",
+    "url": "/static/js/main.0f882073.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
